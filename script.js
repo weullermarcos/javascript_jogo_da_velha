945329ceea6339cc031850a4f6e9ea5865a2b94f
weuller <weullermarcos@hotmail.com>
2021-07-26 11:06:14 -0300

    Finalizando jogo

--- script.js
+++ script.js
@@ -19,7 +19,26 @@ reset();
 //criado evento de click para resetar o jogo
 document.querySelector('.reset').addEventListener('click', reset);
 
+//adicionando evento de click aos itens
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick);
+});
+
 //Funções:
+
+function itemClick(event){
+
+    let item = event.target.getAttribute('data-item');
+    // console.log('clicou em ', item);
+
+    //se o item não estiver preenchido recebe o player
+    if(playing && square[item] === ''){
+        square[item] = player;
+        renderSquare(); //renderiza a tela
+        togglePlayer(); //troca o jogador que está jogando
+    }
+
+}
 
 function reset(){
 
@@ -41,14 +60,22 @@ function reset(){
 
 }
 
+function togglePlayer(){
+
+    player = (player === 'x') ? 'o' : 'x';
+    renderInfo();
+}
+
 function renderSquare(){
 
     for(let i in square){
 
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
-
     }
+
+    checkGame();
+
 }
 
 function renderInfo(){
@@ -56,3 +83,51 @@ function renderInfo(){
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
 }
+
+function checkGame(){
+
+    if(checkWinnerFor('x')){
+        warning = 'O "x" venceu';
+        playing = false;
+    }
+    else if(checkWinnerFor('o')){
+        warning = 'O "o" venceu';
+        playing = false;
+    }
+    else if (isFull()){
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player){
+
+    let pos = [
+        'a1,a2,a3', 'b1,b2,b3', 'c1,c2,c3', //vitorias na horizontal
+        'a1,b1,c1', 'a2,b2,c2', 'a3,b3,c3', //vitorias na vertical
+        'a1,b2,c3', 'a3,b2,c1' //vitorias na diagonal
+    ];
+
+    for(let w in pos){
+
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every(option => square[option] === player);
+
+        if(hasWon){
+            return true;
+        }
+    }
+    return false;
+}
+
+function isFull(){
+
+    for(let i in square){
+        if(square[i] === ''){
+            return false;
+        }
+    }
+
+    return  true;
+}
+
